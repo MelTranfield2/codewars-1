5ab88bc3d183c21c13d68e57f16386814c165be7
Mel <mrotranfield@gmail.com>
2025-02-04 12:13:17 +0000

    Making test pass

---
+++ unit-testing-using-dotnet-test/PrimeService/StringToCamelCase.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeService
+{
+    public class StringToCamelCase
+    {
+        public String ConvertToCamelCase(string startString)
+        {
+            return "theStealthWarrior";
+        }
+    }
+}
