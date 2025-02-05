8fbf1e41fdcba99485052c4eb69d9d37e479a172
Mel <mrotranfield@gmail.com>
2025-02-05 16:56:32 +0000

    adding all test cases for GoodvsEvil

--- unit-testing-using-dotnet-test/PrimeService.Tests/GoodVsEvilTest.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/GoodVsEvilTest.cs
@@ -10,12 +10,29 @@ namespace PrimeService.Tests
         GoodVsEvil goodVsEvil = new GoodVsEvil();
 
         [Fact]
-         public void ShouldReturnGoodOverEvil()
+        public void ShouldReturnGoodOverEvil()
         {
             String countGood = "0 0 0 0 0 10";
             String countEvil = "0 1 1 1 1 0 0";
             var result = goodVsEvil.returnGoodVsEvil(countGood, countEvil);
             Assert.Equal("Battle Result: Good triumphs over Evil", result);
+        }
+        [Fact]
+        public void ShouldReturnEvilOverGood()
+        {
+            String countGood = "1 1 1 1 1 1";
+            String countEvil = "1 1 1 1 1 1 1";
+            var result = goodVsEvil.returnGoodVsEvil(countGood, countEvil);
+            Assert.Equal("Battle Result: Evil eradicates all trace of Good", result);
+        }
+
+        [Fact]
+        public void ShouldReturnBothEqual()
+        {
+            String countGood = "1 0 0 0 0 0";
+            String countEvil = "1 0 0 0 0 0 0";
+            var result = goodVsEvil.returnGoodVsEvil(countGood, countEvil);
+            Assert.Equal("Battle Result: No victor on this battle field", result);
         }
     }
 }
