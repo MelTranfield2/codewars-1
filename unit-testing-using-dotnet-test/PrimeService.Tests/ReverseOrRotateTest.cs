709978deba8c4e28a3d7854d80413accd7c08e65
Mel <mrotranfield@gmail.com>
2025-02-06 18:35:20 +0000

    More test cases for ReverseOrRotate

--- unit-testing-using-dotnet-test/PrimeService.Tests/ReverseOrRotateTest.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/ReverseOrRotateTest.cs
@@ -10,13 +10,32 @@ namespace PrimeService.Tests
 
         ReverseOrRotate reverseOrRotate = new ReverseOrRotate();
 
-       [Fact]
-        public void ShouldReturnStringRotatedToLeft()
+        [Fact]
+        public void ShouldReturn234561876549()
         {
             string inputString = "123456987654";
             int inputSz = 6;
             var result = reverseOrRotate.RevRot(inputString, inputSz);
             Assert.Equal("234561876549", result);
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyString()
+        {
+            string inputString = "";
+            int inputSz = 8;
+            var result = reverseOrRotate.RevRot(inputString, inputSz);
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public void ShouldReturn0365065073456944()
+        {
+            string inputString = "563000655734469485";
+            int inputSz = 4;
+            var result = reverseOrRotate.RevRot(inputString, inputSz);
+            Assert.Equal("0365065073456944", result);
 
         }
     }
