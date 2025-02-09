1f14533747444cbfb9e2c0a1830e9c470a7905c7
Mel <mrotranfield@gmail.com>
2025-02-09 13:27:46 +0000

    CountingDuplicatesTest

--- unit-testing-using-dotnet-test/PrimeService.Tests/CountingDuplicatesTest.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/CountingDuplicatesTest.cs
@@ -7,12 +7,14 @@ namespace PrimeService.Tests
 {
     public class CountingDuplicatesTest
     {
+        public CountingDuplicates countingDuplicates = new CountingDuplicates();
+
         [Fact]
         public void ShouldReturn0EmptyString()
         {
             string inputString = "";
             var result = countingDuplicates.getDuplicates(inputString);
-            Assert.Equal("0", result);
+            Assert.Equal(0, result);
         }
 
         [Fact]
@@ -20,7 +22,7 @@ namespace PrimeService.Tests
         {
             string inputString = "abcde";
             var result = countingDuplicates.getDuplicates(inputString);
-            Assert.Equal("0", result);
+            Assert.Equal(0, result);
         }
     }
 }
