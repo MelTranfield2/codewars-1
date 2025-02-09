c3c9e371eb2ef172661dbc0a51b67a69761c1835
Mel <mrotranfield@gmail.com>
2025-02-09 14:56:46 +0000

    first two tests for CountingDuplicates passing

--- unit-testing-using-dotnet-test/PrimeService.Tests/CountingDuplicatesTest.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/CountingDuplicatesTest.cs
@@ -24,5 +24,21 @@ namespace PrimeService.Tests
             var result = countingDuplicates.getDuplicates(inputString);
             Assert.Equal(0, result);
         }
+
+        [Fact]
+        public void ShouldReturn2()
+        {
+            string inputString = "aabbcde";
+            var result = countingDuplicates.getDuplicates(inputString);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void ShouldIgnoreCase()
+        {
+            string inputString = "aabBcde";
+            var result = countingDuplicates.getDuplicates(inputString);
+            Assert.Equal(2, result);
+        }
     }
 }
---
+++ unit-testing-using-dotnet-test/PrimeService/CountingDuplicates.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeService
+{
+    public class CountingDuplicates
+    {
+        // Write a function that will return the count of distinct case-insensitive
+        // alphabetic characters and numeric digits that occur more than once in the input string.
+        // The input string can be assumed to contain only alphabets (both uppercase and 
+        // lowercase) and numeric digits.
+        public int getDuplicates(string inputString)
+        {
+            //iterate through, do contains check in LINQ or Any
+            int countDuplicates = 0;
+            var nonDuplicates = inputString.Distinct();
+            var chars = inputString;
+            if (chars.Any(ch => !nonDuplicates.Contains(ch)))
+            {
+                countDuplicates = inputString.Length - nonDuplicates.Count();
+            }
+            return countDuplicates;
+        }
+    }
+}
