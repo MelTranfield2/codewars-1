9016b8dcff75748706304fd5450c79fa5ae1c01f
Mel <mrotranfield@gmail.com>
2025-02-10 23:11:21 +0000

    all AreWeAlternateTests passing

--- unit-testing-using-dotnet-test/PrimeService.Tests/AreWeAlternateTest.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/AreWeAlternateTest.cs
@@ -11,9 +11,26 @@ namespace PrimeService.Tests
         AreWeAlternate areWeAlternate = new AreWeAlternate();
 
         [Fact]
-        public void ShouldReturnInd02()
+        public void ShouldReturnTrue()
         {
             string inputWord = "amazon";
+            var result = areWeAlternate.IsAlt(inputWord);
+            Assert.Equal(true, result);
+        }
+
+        [Fact]
+        public void ShouldReturnFalse()
+        {
+            string inputWord = "apple";
+            var result = areWeAlternate.IsAlt(inputWord);
+            Assert.Equal(false, result);
+        }
+
+
+        [Fact]
+        public void ShouldReturnTrueBanana()
+        {
+            string inputWord = "banana";
             var result = areWeAlternate.IsAlt(inputWord);
             Assert.Equal(true, result);
         }
--- unit-testing-using-dotnet-test/PrimeService/AreWeAlternate.cs
+++ unit-testing-using-dotnet-test/PrimeService/AreWeAlternate.cs
@@ -9,6 +9,12 @@ namespace PrimeService
     {
         public bool IsAlt(string word)
         {
+            //put aeiou in an array or dict
+            //loop through string, or use linq where/select
+            //do a check if any of the values , int i + 1 are equal to one of the vowels in the array
+            //check the index, only if int i + 1 index is equal to one of the values in the array then it passes
+            //check the previous vowel
+
             HashSet<char> vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
 
             bool currentCharIsVowel = vowels.Contains(word[0]);
